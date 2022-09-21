935f4cff7f4762ce683ff2b49889f6c8a7c5a744
appll <appll@DESKTOP-9UQIVVD>
2022-09-21 23:09:06 +0900

    자바 공부 테스트

--- Javastudy/src/Mw01.java
+++ Javastudy/src/Mw01.java
@@ -4,6 +4,7 @@ public class Mw01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Minwoo");
+		System.out.println("Hello Yuri");
 	}
 
 }
